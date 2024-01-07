16434331c68abaa6a8ef5465f2164eb34202619d
Jaka Maver <jm18457@gmail.com>
2024-01-07 15:42:06 +0100

    feat(Day-01): Complete
    Squashed commit of the following:
    
    commit 76603048e6eff56d44ded79d1fe047aa1496a3da
    Author: Jaka Maver <jm18457@gmail.com>
    Date:   Sun Jan 7 15:41:36 2024 +0100
    
        solve p2
    
    commit 19119df1cec0b93de52f4cbccf0abbdb215ef6a4
    Author: Jaka Maver <jm18457@gmail.com>
    Date:   Sun Jan 7 15:24:08 2024 +0100
    
        solve p1

---
+++ day01/solution.py
@@ -0,0 +1,37 @@
+def parse():
+    with open('input') as f:
+        data = f.read()
+    numbers = [int(v) for v in data.split('\n')]
+    return numbers
+
+
+def p1():
+    numbers = parse()
+    set_of_numbers = set(numbers)
+    for number in numbers:
+        wanted = 2020 - number
+        if wanted in set_of_numbers:
+            return wanted * number
+
+
+def p2():
+    numbers = parse()
+    map_of_partial_sums = {}
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            partial = numbers[i] + numbers[j]
+            map_of_partial_sums[partial] = (numbers[i], numbers[j])
+
+    for number in numbers:
+        wanted = 2020 - number
+        if wanted in map_of_partial_sums and number not in map_of_partial_sums[wanted]:
+            a, b = map_of_partial_sums[wanted]
+            return a * b * number
+
+
+def main():
+    print(f'p2: {p1()}')
+    print(f'p2: {p2()}')
+
+
+main()
---
+++ template/solution.py
@@ -0,0 +1,22 @@
+def parse():
+    with open('input') as f:
+        data = f.read()
+    return data
+
+
+def p1():
+    data = parse()
+    return
+
+
+def p2():
+    data = parse()
+    return
+
+
+def main():
+    print(f'p2: {p1()}')
+    print(f'p2: {p2()}')
+
+
+main()
